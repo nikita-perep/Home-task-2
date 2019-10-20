88b10d668e24586f923198495091f5fd992856d6
= <=>
2019-10-20 16:57:05 +0400

    Calculator

---
+++ calculator.js
@@ -0,0 +1,29 @@
+function sum(a,b) {
+    return result = a + b;
+}
+console.log(sum(2,3));
+
+function subtraction(a,b) {
+    return result = a - b;
+}
+console.log(subtraction(2,3));
+
+function addition(a,b) {
+    return result = a + b;
+}
+console.log(addition(2,3));
+
+function multiplication(a,b) {
+    return result = a * b;
+}
+console.log(multiplication(2,3));
+
+function division(a,b) {
+    return result = a / b;
+}
+console.log(division(2,3));
+
+function exponentiation(a,b) {
+return result = a ** b;
+}
+console.log(exponentiation(2,3));
