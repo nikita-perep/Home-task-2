8848d6131a2aa23926f92a185994f62353276538
= <=>
2019-10-20 18:46:19 +0400

    Calculator

--- calculator.js
+++ calculator.js
@@ -27,3 +27,21 @@ function exponentiation(a,b) {
 return result = a ** b;
 }
 console.log(exponentiation(2,3));
+
+ // function pow(a,b) {
+ //     let result = 1;
+ //     for (let i = 0; i < b; i++) {
+ //         result *= a;
+ //     }
+ //     return result
+ // }
+ // console.log(pow(2,3));
+
+ function pow(a,b) {
+     if (b === 1) {
+         return a;
+     } else {
+         return a * pow(a, b - 1);
+     }
+ }
+ console.log(pow(2,4));
